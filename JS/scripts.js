47cc10167d19ae0178e1b2ba801d862854fefc77
roberto <rdgc18@gmail.com>
2021-04-24 16:19:57 -0500

    FINAL

--- JS/scripts.js
+++ JS/scripts.js
@@ -15,3 +15,56 @@ for (const item of menu_item) {
     menu.classList.toggle('visible');
   })
 }
+
+const galleryItems=document.querySelectorAll('.gallery-item');
+const lightbox=document.getElementById('lightbox');
+const lightboxImg=lightbox.querySelector('.lightbox-img');
+const lightboxText=lightbox.querySelector('.caption-text');
+const lightboxCounter=lightbox.querySelector('.caption-counter');
+const lightboxClose=lightbox.querySelector('.lightbox-close');
+
+let itemIndex=0;
+
+for(let i=0;i<galleryItems.length;i++){
+        galleryItems[i].addEventListener("click",()=>{
+        itemIndex=i;
+        changeItem();
+        toggleLightbox();
+    })
+}
+
+const prevItem=document.getElementById('prev-item');
+const nextItem=document.getElementById('next-item');
+
+nextItem.addEventListener("click",()=>{
+    if(itemIndex==galleryItems.length-1)
+        itemIndex=0;
+    else
+        itemIndex++;
+    changeItem();
+})
+
+prevItem.addEventListener("click",()=>{
+    if(itemIndex==0)
+        itemIndex=galleryItems.length-1;
+    else
+        itemIndex--;
+    changeItem();
+})
+
+const toggleLightbox=()=>{
+    lightbox.classList.toggle("open");
+}
+
+const changeItem=()=>{
+    imgSrc=galleryItems[itemIndex].querySelector(".gallery-item img").getAttribute("src");
+    lightboxImg.src=imgSrc;
+    lightboxText.innerHTML='<h2>Title</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea molestias magni. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea molestias magni.</p>'
+    lightboxCounter.textContent=`${itemIndex+1} / ${galleryItems.length}`;
+}
+
+lightbox.addEventListener('click',(e)=>{
+    if(e.target===lightboxClose || e.target===lightbox)
+        toggleLightbox();
+
+})
